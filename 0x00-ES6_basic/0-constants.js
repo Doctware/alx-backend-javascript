b0090e7d1f5b96a1465baef7036629a0b1786c51
Doctware <yusclever9@gmail.com>
2024-10-03 18:37:45 +0100

    I prefer const when i can But sumtimes let is okay

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,15 @@
+export function taskFirst() {
+	const task = 'I prefer const when i can';
+	return task;
+}
+
+export function getLast() {
+	return ' is okay';
+}
+
+export function taskNext() {
+	let combination = 'But sumtimes let';
+	combination += getLast();
+
+	return combination;
+}
