e57a210c2758408c224d0679a78f51cd9a52a842
Doctware <yusclever9@gmail.com>
2024-10-04 23:12:55 +0100

    Let loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let [idx, value] of array.entries()) {
-    array[idx] = appendString + value;
+    array[idx] = appendString + value; // elint-disable-line
   }
 
   return array;
