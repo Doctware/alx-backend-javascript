1375db1921dd7918ac2c444856493ae0c7199661
Doctware <yusclever9@gmail.com>
2024-10-04 23:36:34 +0100

    ES6 method properties

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,11 +1,11 @@
-import getBudgetObject from './7-getBudgetObject.js'; // eslint-disable-line
+import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollar (income) { return `$${income}`; },
-    getIncomeInEuro (income) { return `${income} euros`; }
+    getIncomeInDollar(income) { return `$${income}`; },
+    getIncomeInEuro(income) { return `${income} euros`; },
   };
 
   return fullBudget;
