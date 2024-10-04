ea32e6ebeab5838042beeac6325b8af4e8ce42d1
Doctware <yusclever9@gmail.com>
2024-10-04 18:37:27 +0100

    Interpolation

---
+++ 0x00-ES6_basic/6-string-interpolation.js
@@ -0,0 +1,10 @@
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$199,868',
+    gdp: '$145.2 bill,ion',
+    capita: '$178,479',
+  };
+
+  return `As of ${year}, it wa the seventh-highest income county in the United States, with a per capita personal icome of ${budget.icome}. As of 2015, San Francisco proper hasd a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}`;
+}
