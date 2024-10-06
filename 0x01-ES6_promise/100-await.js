131bd516ec020700c9a65514c2b4d3c536523cc1
Doctware <yusclever9@gmail.com>
2024-10-06 04:34:30 +0100

    awaiting thw good news

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function asyncUploadUser() {
+  const response = {
+    photo: null,
+    user: null,
+  };
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+
+    response.photo = photo;
+    response.user = user;
+  } catch (error) {
+    return response;
+  }
+  return response;
+}
