615c3fd2e40ff16f35f9b3701cc401f6331ea409
Doctware <yusclever9@gmail.com>
2024-10-06 04:45:08 +0100

    :)

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const results = await Promise.allSettled([
     signUpUser(firstName, lastName), // Assuming this returns a Promise
-    uploadPhoto(fileName) // Assuming this returns a Promise
+    uploadPhoto(fileName), // Assuming this returns a Promise
   ]);
-  return results.map(result => {
-    return {
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason
-    };
-  });
+  return results.map((result) => ({
+    status: result.status,
+    value: result.status === 'fulfilled' ? result.value : result.reason,
+  }));
 }
