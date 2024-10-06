a0f0d1206f9c98003f31646ef091ac71ca08b116
Doctware <yusclever9@gmail.com>
2024-10-06 04:37:04 +0100

    throw try

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,10 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
   try {
     const result = mathFunction();
-    queue.push(result);
+    queue.push(result); // Push the result into the queue
   } catch (error) {
-    queue.push(error.message);
+    queue.push(error.toString()); // Convert the error to a string and push it into the queue
   }
   queue.push('Guardrail was processed');
+
   return queue;
 }
