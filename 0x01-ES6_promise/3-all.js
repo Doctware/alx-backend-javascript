b9dd203d3ed9640cc4f6851c051bcb28a57c8843
Doctware <yusclever9@gmail.com>
2024-10-06 03:28:50 +0100

    handule multiole promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+function handuleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoResp, userResp] = results;
+      console.log(`${photoResp.body} ${userResp.firstName} ${userResp.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
+
+export default handuleProfileSignup;
