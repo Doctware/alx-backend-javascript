7a0ee4984b02a3d9355c351413558f49dc489793
Doctware <yusclever9@gmail.com>
2024-10-09 00:06:41 +0100

    geting the list in the array of obj

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,7 +2,7 @@ function getListStudents() {
   const studentList = [];
 
   const ourObj = [
-    { id: 1, firstName: 'Guillame', location: 'San Francisco' },
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
     { id: 2, firstName: 'James', location: 'Columbia' },
     { id: 5, firstName: 'Serena', location: 'San Francisco' },
   ];
