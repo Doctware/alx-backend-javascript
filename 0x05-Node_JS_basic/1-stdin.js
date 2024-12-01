7b64694bc43745d9dbcf068f227ed65ba2762e19
Doctware <yusclever9@gmail.com>
2024-12-01 14:43:14 +0100

    Using process STDIN

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,11 @@
+console.log("Welcome to Holberton School, what is your name?");
+
 process.stdin.on('data', (data) => {
-  console.log('Welcome to Holberton School, what is your name?');
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-
-  console.log('This important software is now closing');
-  process.exit();
+  
+  if (!process.stdin.isTTY) {
+    console.log('This important software is now closing');
+    process.exit();
+  }
 });
