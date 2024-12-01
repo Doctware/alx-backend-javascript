461e5849d79126533d98de66016bd324b05e5da6
Doctware <yusclever9@gmail.com>
2024-12-01 17:44:53 +0100

    reading file sycronously

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,17 +1,14 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   try {
-    // read file syncronously
     const data = fs.readFileSync(path, 'utf-8');
 
     const lines = data.split('\n').filter((line) => line.trim() !== '');
 
-    // const header = lines[0].split(',');
     const students = lines.slice(1).map((line) => line.split(','));
 
     console.log(`Number of students: ${students.length}`);
-    // creating a map to store student by fields
 
     const fields = {};
 
