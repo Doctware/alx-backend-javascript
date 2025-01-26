0b3f3342da9e44cf9ba96e8fcef6125de0c6c5b5
Doctware <yusclever9@gmail.com>
2025-01-26 17:57:00 +0100

    getter and setter

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -7,14 +7,14 @@ export default class ALXCourse {
       throw new TypeError('Length must be a number');
     }
     if (!Array.isArray(students) || !students.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of string');
+      throw new TypeError('Students must be an array of strings');
     }
 
     this._name = name;
     this._length = length;
     this._students = students;
   }
-
+i
   get name() {
     return this._name;
   }
@@ -28,11 +28,11 @@ export default class ALXCourse {
   }
 
   get length() {
-    return this._lenght;
+    return this._length;
   }
 
   set length(newLength) {
-    if (typeof newLength !== 'string') {
+    if (typeof newLength !== 'number') {
       throw new TypeError('Length must be a number');
     }
     this._legth = newLength;
