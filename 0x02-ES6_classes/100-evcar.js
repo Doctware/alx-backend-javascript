dda343661edf9e69c59ccbc0010c377e3d9759b8
Doctware <yusclever9@gmail.com>
2025-01-27 12:55:41 +0100

    car

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,11 +1,14 @@
-export default class Car {
-  constructor(brand, motor, color) {
-    this._brand = brand;
-    this._motor = motor;
-    this._color = color;
+import Car from './10-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color); // Call parent constructor
+    this._range = range; // Set specific attribute for EVCar
   }
 
+  // Override the cloneCar method to return an instance of Car
   cloneCar() {
     return new Car(this._brand, this._motor, this._color);
   }
 }
+
