727ca1ce087320f755525603357571ffb819bf9a
Doctware <yusclever9@gmail.com>
2025-07-10 16:01:06 +0000

    Implemented extending from interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -4,5 +4,19 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
-  [key: string] any;
+  [key: string]: any;
 }
+
+interface Director extends Teacher {
+  numberOfReport: number;
+}
+
+const director: Director = {
+  firstName: "Alani",
+  lastName: "Yusuf",
+  fullTimeEmployee: false,
+  location: "Lagos, Nigeria",
+  numberOfReport: 10,
+}
+
+console.log(director)
