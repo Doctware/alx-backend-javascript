beffa5f7e2c14f35966b9f49e83581f03ae5862d
Doctware <yusclever9@gmail.com>
2025-07-11 10:05:16 +0000

    Implemented class interfaces and constuctor interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -27,7 +27,36 @@ const director: Director = {
   numberOfReport: 10,
 }
 
+interface studentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface sudentConstructor {
+  new (firstName: string, lastName: string): studentClassInterface;
+}
+
+class StudentClass implements studentClassInterface {
+  private firstName: string;
+  private lastName: string;
+  
+  constructor(firstName: string, lastName: string) {
+    this.lastName = lastName;
+    this.firstName = firstName;
+  }
+
+  workOnHomework = (): string => "currently working";
+  displayName = (): string =>{
+    return `${this.firstName}`
+  }
+
+
+}
+
 console.log(director)
 console.log(printTeacher("Olayode", "Yusuf"))
-
-
+console.log("====================================")
+const student = new StudentClass("Olayode", "Yusuf")
+console.log(student)
+console.log(student.workOnHomework());
+console.log(student.displayName());
